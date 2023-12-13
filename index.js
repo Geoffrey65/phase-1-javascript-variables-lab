e50293eb8d282d18a1c9288c520569b280a7100d
Geoffrey <geoffrey>
2023-12-13 17:42:26 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+const companyName = 'Scuber'; // Fixed to match test expectation
+
+let companyCeo = 'Susan Smith';
+let mostProfitableNeighborhood = 'Chelsea';
+
+function makeProfit(amount) {
+  return amount * 1.1;
+}
+
+const profit = makeProfit(100);
+
+console.log(`The company name is ${companyName}.`); // Fixed to match test expectation
+console.log(`The CEO of the company is ${companyCeo}.`);
+console.log(`The most profitable neighborhood is ${mostProfitableNeighborhood}.`);
+console.log(`The profit made is ${profit}`);
